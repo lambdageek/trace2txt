b5a30f99bc4b0c50fbcedff2e81ff1c8aab4adca
Aleksey Kliger <alklig@microsoft.com>
2022-05-03 14:42:44 -0400

    add an event name filter

--- Program.cs
+++ Program.cs
@@ -1,8 +1,49 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Microsoft.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Tracing.EventPipe;
 
 namespace Trace2txt;
+
+public class Filter {
+    public Regex? EventFilter {get; set;}
+    
+    public Filter() {}
+
+    private bool Match (TraceEvent evt)
+    {
+        if (EventFilter == null)
+            return true;
+        return EventFilter.IsMatch (evt.EventName);
+    }
+
+    public void OnTraceEvent(TraceEvent evt)
+    {
+        if (!Match(evt))
+            return;
+        Console.WriteLine (evt.Dump());
+    }
+
+    public void OnUnhandledEvent(TraceEvent evt)
+    {
+        Console.WriteLine("WARNING: unhandled event {0}", evt.Dump());
+    }
+
+    public void OnThreadSample(ClrThreadSampleTraceData data)
+    {
+        if (!Match(data))
+            return;
+        Console.WriteLine("ThreadSample: {0}", data.Dump());
+    }
+
+    public void OnThreadStackWalk(ClrThreadStackWalkTraceData data)
+    {
+        if (!Match(data))
+            return;
+        Console.WriteLine("ThreadStackWalk: {0}", data.Dump());
+    }
+
+}
 
 public class Program
 {
@@ -10,17 +51,24 @@ public class Program
 
     public static int Main(string[] args)
     {
+        const string eventParamPrefix = "--event=";
         Mode mode = Mode.DumpSamples;
         if (args.Length < 1)
         {
-            Console.Error.WriteLine("Usage: trace2txt [--all] <trace-file>");
+            Console.Error.WriteLine("Usage: trace2txt [--all] [--event=<regex>] <trace-file>");
             return 1;
         }
         string? fileName = null;
+        Filter filter = new();
         foreach (var arg in args) {
             if (arg == "--all")
             {
                 mode = Mode.DumpAll;
+                continue;
+            }
+            if (arg.StartsWith(eventParamPrefix))
+            {
+                filter.EventFilter = new Regex(arg[eventParamPrefix.Length..]);
                 continue;
             }
             if (fileName == null)
@@ -44,10 +92,10 @@ public class Program
 
         switch (mode) {
             case Mode.DumpAll:
-                SetupDumpAll(t);
+                SetupDumpAll(t, filter);
                 break;
             case Mode.DumpSamples:
-                SetupDumpSamples(t);
+                SetupDumpSamples(t, filter);
                 break;                
         }
 
@@ -55,47 +103,28 @@ public class Program
         return 0;
     }
 
-    public static void SetupDumpAll(EventPipeEventSource t)
+    public static void SetupDumpAll(EventPipeEventSource t, Filter filter)
     {
-        t.Clr.All += OnTraceEvent;
-        t.AllEvents += OnTraceEvent;
+        t.Clr.All += filter.OnTraceEvent;
+        t.AllEvents += filter.OnTraceEvent;
         Microsoft.Diagnostics.Tracing.Parsers.Clr.ClrRundownTraceEventParser clrRundownParser = new(t);
         SampleProfilerTraceEventParser sampleProfilerParser = new(t);
-        clrRundownParser.All += OnTraceEvent;
-        sampleProfilerParser.All += OnTraceEvent;
+        clrRundownParser.All += filter.OnTraceEvent;
+        sampleProfilerParser.All += filter.OnTraceEvent;
 
     }
 
-    public static void SetupDumpSamples(EventPipeEventSource t)
+    public static void SetupDumpSamples(EventPipeEventSource t, Filter filter)
     {
         SampleProfilerTraceEventParser sampleProfilerParser = new(t);
         Microsoft.Diagnostics.Tracing.Parsers.Clr.ClrRundownTraceEventParser clrRundownParser = new(t);
 
-        t.Clr.All += OnTraceEvent;
-        clrRundownParser.All += OnTraceEvent;
-        sampleProfilerParser.ThreadSample += OnThreadSample;
-        sampleProfilerParser.ThreadStackWalk += OnThreadStackWalk;
-        t.UnhandledEvents += OnUnhandledEvent;
+        t.Clr.All += filter.OnTraceEvent;
+        clrRundownParser.All += filter.OnTraceEvent;
+        sampleProfilerParser.ThreadSample += filter.OnThreadSample;
+        sampleProfilerParser.ThreadStackWalk += filter.OnThreadStackWalk;
+        t.UnhandledEvents += filter.OnUnhandledEvent;
     }
 
-    public static void OnTraceEvent(TraceEvent evt)
-    {
-        Console.WriteLine (evt.Dump());
-    }
-
-    public static void OnUnhandledEvent(TraceEvent evt)
-    {
-        Console.WriteLine("WARNING: unhandled event {0}", evt.Dump());
-    }
-
-    public static void OnThreadSample(ClrThreadSampleTraceData data)
-    {
-        Console.WriteLine("ThreadSample: {0}", data.Dump());
-    }
-
-    public static void OnThreadStackWalk(ClrThreadStackWalkTraceData data)
-    {
-        Console.WriteLine("ThreadStackWalk: {0}", data.Dump());
-    }
 
 }
